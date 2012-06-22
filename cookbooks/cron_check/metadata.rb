79cfc7a79cd99fc36df10fd7f33dece6dde8c8a9
Evan Machnic <emachnic@broadmac.net>
2012-06-22 14:44:53 -0400

    Fixed foodcritic warnings on cron_check cookbook

---
+++ cookbooks/cron_check/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Engine Yard"
+maintainer_email ""
+license          ""
+description      "Adds cron entry to check the timestamp on cron"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          ""
--- cookbooks/cron_check/recipes/default.rb
+++ cookbooks/cron_check/recipes/default.rb
@@ -1,13 +1,13 @@
 # Recipe for testing cron is working properly.
 #
 cron "timestamp_check" do
-  user node[:owner_name]
+  user node['owner_name']
   minute "*"
   hour "*"
   day "*"
   month "*"
   weekday "*"
-  command "touch /home/#{node[:owner_name]}/cron.timestamp.check"
+  command "touch /home/#{node['owner_name']}/cron.timestamp.check"
   action :create
 end
 
@@ -18,7 +18,7 @@ template "/etc/monit.d/cron_timestamps.monitrc" do
   group "root"
   mode 0655
   variables({
-  :owner => node[:owner_name]
+  :owner => node['owner_name']
   })
 end
 
