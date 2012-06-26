3043b1a52e0b6838a4e59430ada4f1b528005e7c
Evan Machnic <emachnic@broadmac.net>
2012-06-25 21:30:47 -0400

    Fixed foodcritic warnings for eybackup_verbose

---
+++ cookbooks/eybackup_verbose/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Engine Yard"
+maintainer_email ""
+license          ""
+description      "Logs verbose output from eybackup and ey-snapshot"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          ""
--- cookbooks/eybackup_verbose/recipes/default.rb
+++ cookbooks/eybackup_verbose/recipes/default.rb
@@ -1,17 +1,17 @@
 # Copyright Engine Yard - 2010
 
 #set backup interval
-cron_hour = if node[:backup_interval].to_s == '24'
+cron_hour = if node['backup_interval'].to_s == '24'
               "1"    # 0100 Pacific, per support's request
               # NB: Instances run in the Pacific (Los Angeles) timezone
-            elsif node[:backup_interval]
-              "*/#{node[:backup_interval]}"
+            elsif node['backup_interval']
+              "*/#{node['backup_interval']}"
             else
               "1"
             end
 
 
-if ['solo', 'db_master'].include?(node[:instance_role])
+if ['solo', 'db_master'].include?(node['instance_role'])
   cron "eybackup" do
     action :delete
   end
@@ -23,7 +23,7 @@ if ['solo', 'db_master'].include?(node[:instance_role])
     month    '*'
     weekday  '*'
     command  "/usr/local/ey_resin/bin/eybackup >> /db/eybackup.log"
-    not_if { node[:backup_window].to_s == '0' }
+    not_if { node['backup_window'].to_s == '0' }
   end  
 
   cron "ey-snapshots" do
@@ -38,6 +38,6 @@ if ['solo', 'db_master'].include?(node[:instance_role])
     month    '*'
     weekday  '*'
     command  "ey-snapshots --snapshot >> /db/ey-snapshots.log"
-    not_if { node[:backup_window].to_s == '0' }
+    not_if { node['backup_window'].to_s == '0' }
   end
 end
