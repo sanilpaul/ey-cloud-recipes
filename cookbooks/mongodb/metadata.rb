bb01ab82c2dcf1e7140dc7b7b274901aa4f429c6
Evan Machnic <emachnic@broadmac.net>
2012-06-26 00:51:15 -0400

    Fixed foodcritic warnings for MongoDB

---
+++ cookbooks/mongodb/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Engine Yard"
+maintainer_email ""
+license          ""
+description      "Cookbook for setting up MongoDB on Engine Yard Cloud"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          ""
